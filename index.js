27e80ed4809948d3ba3f452f0832a9af8a7e739e
plassi <puurunen.lassi@gmail.com>
2020-02-23 13:29:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+
+morgan.token('person', (req, res) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 4
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Harry Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  },
+]
+
+// Functions
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+// Phonebook info
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p> `)
+})
+
+// Persons API
+
+app.post('/api/persons', (req, res) => {
+ 
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  }
+
+  if (persons.map(person => person.name).includes(body.name)) {
+    return res.status(400).json({ 
+      error: 'name is already in the list' 
+    })
+  }
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: getRandomIntInclusive(0, 1600000)
+  }
+  
+  persons = persons.concat(person)
+  
+  res.json(person)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+  
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+  console.log(persons);
+  
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
